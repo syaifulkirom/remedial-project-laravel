fe9519da8004382642f9595e39845bb3eb02c2b2
syaifulkirom <syaifulqirom2@gmail.com>
2020-08-07 17:55:32 +0700

    dokumen hasil testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,7 +76,7 @@
             <input type="hidden" name="user_id" value="{{ $user->id }}">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Profile</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
